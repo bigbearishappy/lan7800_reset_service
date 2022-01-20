31ff416d5f1e88cf25c052d2e8eb93b8a5dce39c
bbear <953308023@qq.com>
2022-01-20 02:50:50 +0000

    add the input param for script

--- lan7800_ESD_test.sh
+++ lan7800_ESD_test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+SERVER_IP=$1
+
 # Check root
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root (use sudo)" 1>&2
   exit 1;
 fi
 
+# Check param
+if [ ! -n "$SERVER_IP" ]; then
+   echo "please input the server ip address(example: sudo ./lan7800_ESD_test.sh 192.168.100.0)"
+   exit 1;
+fi
+
 echo ethernet client test
+echo server ip: $SERVER_IP
 
 ./lan7800_error_reset &
 
-iperf3 -c 192.168.113.100 -p 1000 -b 1000M
+iperf3 -c $SERVER_IP -p 1000 -b 1000M
